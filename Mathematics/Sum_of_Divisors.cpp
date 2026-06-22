67de32085099886a2b43312f46c45df07cdb7cd4
Dinkar Upadhyay <dinkarupadhyay2906@gmail.com>
2026-06-22 22:38:19 +0530

    maths sum of divisors

--- Mathematics/Sum_of_Divisors.cpp
+++ Mathematics/Sum_of_Divisors.cpp
@@ -3,21 +3,50 @@ using namespace std;
 
 using intt = long long;
 
+const intt MOD = 1e9 + 7;
+const intt INV2 = 500000004;
+
+intt sumRange(intt l, intt r) {
+
+    l %= MOD;
+    r %= MOD;
+
+    intt cnt = (r - l + 1) % MOD;
+    intt sum = (l + r) % MOD;
+
+    return (((sum * cnt) % MOD) * INV2) % MOD;
+}
+
 void solve() {
-    
+
+    intt n;
+    cin >> n;
+
+    intt ans = 0;
+
+    intt l = 1;
+
+    while(l <= n) {
+
+        intt q = n / l;
+
+        intt r = n / q;
+
+        intt cur = sumRange(l, r);
+
+        ans = (ans + (q % MOD) * cur) % MOD;
+
+        l = r + 1;
+    }
+
+    cout << ans << '\n';
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    intt n;
-    cin>>n;
-    
-
-    // even after this methof of sigma(d=1 to n(d*floor(n/d)))---->test size 10^12 still 0(n) not good
-    // Harmonic Series 
-
+    solve();
 
     return 0;
 }
