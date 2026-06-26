839c6838fe985ee06f6cdeabff3a2e0b26928f4d
Dinkar Upadhyay <dinkarupadhyay2906@gmail.com>
2026-06-27 03:23:05 +0530

    sorting

--- sorting-and-searching/Tasks_and_Deadlines.cpp
+++ sorting-and-searching/Tasks_and_Deadlines.cpp
@@ -1,20 +1,41 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 using intt = long long;
 
-void solve() {
-    
+void solve()
+{
+    intt n;
+    cin >> n;
+
+    vector<pair<intt,intt>> a(n);
+
+    for (intt i = 0; i < n; i++)
+    {
+        cin >> a[i].first >> a[i].second;
+    }
+
+    sort(a.begin(), a.end());
+
+    intt time = 0;
+    intt ans = 0;
+
+    for (intt i = 0; i < n; i++)
+    {
+        time += a[i].first;
+        ans += a[i].second - time;
+    }
+
+    cout << ans << '\n';
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t = 1;
-    // cin >> t;
-
-    while (t--) solve();
+    solve();
 
     return 0;
 }
